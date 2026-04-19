67850792499aacfc0117ccea29c468211144f0c8
KartikeyRaghav <sse240021008@iiti.ac.in>
2026-04-19 17:13:41 +0530

    Added resend for email

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -1,19 +1,27 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  host: "Gmail",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "Gmail",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.SMTP_EMAIL,
+//     pass: process.env.SMTP_PASSWORD,
+//   },
+// });
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
   try {
-    const info = await transporter.sendMail({
-      from: '"Printex" modisoftech@gmail.com',
+    // const info = await transporter.sendMail({
+    //   from: '"Printex" modisoftech@gmail.com',
+    //   to,
+    //   subject,
+    //   text,
+    // });
+    resend.emails.send({
+      from: "onboarding@resend.dev",
       to,
       subject,
       text,
